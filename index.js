02b45edd9cc2da72ff8bdaa9ec865c7900aef13f
MR Masum <mrmasum820@gmail.com>
2024-03-20 02:55:58 +0600

    news server ready

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const categories = require("./data/categories.json");
+const news = require("./data/news.json");
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+// database connection uri
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lzjopzw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    const db = client.db("dragon_news");
+    const categoriesCollection = db.collection("categories");
+    const newsCollection = db.collection("news");
+
+    // get all news
+    app.get("/all-news", async (req, res) => {
+      const allNews = await newsCollection.find({}).toArray();
+      res.send({ status: true, message: "success", data: allNews });
+    });
+
+    // get specific news
+    app.get("/news/:id", async (req, res) => {
+      const id = req.params.id;
+      const news = await newsCollection.findOne({ _id: new ObjectId(id) });
+      res.send({ status: true, message: "success", data: news });
+    });
+
+    // get all categories
+    app.get("/categories", async (req, res) => {
+      const categories = await categoriesCollection.find({}).toArray();
+      res.send({ status: true, message: "success", data: categories });
+    });
+
+    // get specific categories
+    app.get("/news", async (req, res) => {
+      const name = req.query.category;
+      let newses = [];
+      if (name == "all-news") {
+        newses = await newsCollection.find({}).toArray();
+        return res.send({ status: true, message: "success", data: newses });
+      }
+      newses = await newsCollection
+        .find({ category: { $regex: name, $options: "i" } })
+        .toArray();
+      res.send({ status: true, message: "success", data: newses });
+    });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Dragon News Server!");
+});
+
+app.listen(port, () => {
+  console.log(`API is running on ${port}`);
+});
